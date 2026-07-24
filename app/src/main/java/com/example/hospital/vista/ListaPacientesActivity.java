64883a4acc1613ac9b4f36780efcf7d5ad1facfe
efrenavecillas4 <jitoefren@gmail.com>
2026-07-24 02:02:45 -0600

    arreglo de ListaPacientesActivity

--- app/src/main/java/com/example/hospital/vista/ListaPacientesActivity.java
+++ app/src/main/java/com/example/hospital/vista/ListaPacientesActivity.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 public class ListaPacientesActivity extends AppCompatActivity
         implements PacienteAdapter.OnPacienteSelectedListener {
-
+//hola
     private RecyclerView rvPacientes;
     private TextView tvSinPacientes;
     private Button btnVolver;
